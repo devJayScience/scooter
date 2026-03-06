15cda488686149bcb94a2fc6f80fae88e700b948
devJayScience <jose0106gamer@gmail.com>
2026-03-06 12:34:39 -0500

    vs_2

---
+++ .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Jose/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Jose/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
